65b979e94648a27f58372a4105c4bef7e8295dd0
Artur <40637232+Ariarchi@users.noreply.github.com>
2021-09-23 17:19:02 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ $(document).ready(function () {
   var counterUp = $(".counter-up");
   var counterDown= $(".counter-down");
   var floorPath = $(".home__image path");
+  var modal = $(".modal");
+  var modalCloseButton = $(".modal__close-button");
+  var viewFlatsButton = $(".view-flats");
+  var modalPath = $(".flats path");
 
   //Функция при навелении мышки на этаж
   floorPath.on("mouseover", function () {
@@ -10,6 +14,10 @@ $(document).ready(function () {
     currentFloor = $(this).attr("data-floor");
     $(".counter").text(currentFloor);
   });
+
+  floorPath.on("click", toggleModal); /*При клике на этаж, вызывается окно*/
+  modalCloseButton.on("click", toggleModal); /*При клике на кнопку, закрывает окно */
+  viewFlatsButton.on("click", toggleModal);
 
   //Функция при клике мышки на стрелку вверх
   counterUp.on("click", function () {
@@ -33,4 +41,14 @@ $(document).ready(function () {
     }
   });
 
+  function toggleModal() { //функция открыть/закрыть окно
+    modal.toggleClass("is__open");
+  }
+
+  modalPath.on("mouseover", function () {
+    modalPath.removeClass("current-flats");
+    currentFloor = $(this).attr("data-floor");
+    $(".counter").text(currentFloor);
+  });
+  
 });
